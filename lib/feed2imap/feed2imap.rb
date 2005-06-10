597855ef32b7831de0b5abbd6b816a4eb51a59e8
lnu <lnu@f70e237a-67f3-0310-a06c-d2b8a7116972>
2005-06-10 12:05:09 +0000

    git-svn-id: svn+ssh://svn.gna.org/svn/feed2imap/trunk/feed2imap@31 f70e237a-67f3-0310-a06c-d2b8a7116972

--- lib/feed2imap/feed2imap.rb
+++ lib/feed2imap/feed2imap.rb
@@ -19,15 +19,15 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =end
 
+# Feed2Imap version
+F2I_VERSION = '0.4'
+
 require 'feed2imap/config'
 require 'feed2imap/cache'
 require 'feed2imap/channel'
 require 'feed2imap/httpfetcher'
 require 'logger'
 require 'thread'
-
-# Feed2Imap version
-F2I_VERSION = '0.4'
 
 class Feed2Imap
   def initialize(verbose, cacherebuild, configfile)
@@ -110,8 +110,8 @@ class Feed2Imap
       next if f.body.nil? # means 304
       begin
         channel = Channel::new(f.body)
-      rescue
-        @logger.fatal("Error while parsing #{f.name}: #{$!}")
+      rescue Exception => e
+        @logger.fatal("Error while parsing #{f.name}: #{e}")
         next
       end
       begin
