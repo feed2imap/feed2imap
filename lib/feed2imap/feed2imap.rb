67a850d601826662ab4017f4187466166de6ca52
lnu <lnu@f70e237a-67f3-0310-a06c-d2b8a7116972>
2005-09-19 14:16:54 +0000

    v 0.5
    git-svn-id: svn+ssh://svn.gna.org/svn/feed2imap/trunk/feed2imap@64 f70e237a-67f3-0310-a06c-d2b8a7116972

--- lib/feed2imap/feed2imap.rb
+++ lib/feed2imap/feed2imap.rb
@@ -18,7 +18,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 =end
 
 # Feed2Imap version
-F2I_VERSION = '0.4'
+F2I_VERSION = '0.5'
 
 require 'feed2imap/config'
 require 'feed2imap/cache'
